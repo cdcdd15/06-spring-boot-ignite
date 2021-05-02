107c715ce0e2ed747f840ce76a43319f36ba1dfd
Cosmin Dinu <cdcdd15@gmail.com>
2021-05-02 09:58:11 +0300

    Hello endpoint works.

---
+++ src/main/java/com/cosmin/ignite/todo/EmployeeIgniteService.java
@@ -0,0 +1,8 @@
+package com.cosmin.ignite.todo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeIgniteService{
+//	List<Todo> findByUsername(String username);
+}
--- src/main/java/com/cosmin/ignite/todo/TodoJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cosmin.ignite.todo;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoJpaRepository{
-//	List<Todo> findByUsername(String username);
-}
